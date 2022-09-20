e77b6f15eb2135dc919a008e00918c34c0528e71
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-09-20 15:35:16 +0530

    completed

--- deploy.js
+++ deploy.js
@@ -3,8 +3,14 @@ const fs = require('fs-extra');
 require("dotenv").config();
 
 async function main() {
-    const providers = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, providers);
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    // const encryptedJson = fs.readFileSync("./encryptedKey.js", "utf8");
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    //     encryptedJson,
+    //     process.env.PRIVATE_KEY_PASSWORD
+    // )
+    // wallet = await wallet.connect(provider);
     const abi = fs.readFileSync("./bin/SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./bin/SimpleStorage.bin", "utf8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
